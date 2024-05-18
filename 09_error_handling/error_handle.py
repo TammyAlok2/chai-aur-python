fc4800d24d0b2c719ac9acbe16c806c90d835934
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-05-18 18:00:58 +0530

    Python project completd 1

--- 09_error_handling/error_handle.py
+++ 09_error_handling/error_handle.py
@@ -1,9 +1,9 @@
-file = open('youtube.txt', 'w')
+file = open('youtube.txt','w')
 
 try:
-    file.write('chai aur code')
+    file.write("Chai aur coding ")
 finally:
-    file.close()
-
-with open('youtube.txt', 'w') as file:
-    file.write('chai aur python')
+    file.close()    
+    
+with open('youtube.txt','w')as file:
+    file.write(("chai aur python "))    
